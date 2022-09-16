215f7d7ed8c7f66b9fb2d1c1b85af9f76e8f5819
Yanyeyeye <1024079655@qq.com>
2022-09-17 00:27:51 +0800

    style: adjust index page style

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@
 </script>
 
 <template>
-  <main font-sans text="center gray-700 dark:gray-200">
-    <Footer />
+  <main
+    font-sans
+    mb-5
+    text="center gray-700 dark:gray-200"
+  >
     <router-view />
   </main>
+  <Footer />
 </template>
 
 <style scoped>
--- src/components/system/Footer.vue
+++ src/components/system/Footer.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <nav text-xl mt-6 inline-flex gap-2>
+  <nav text-xl mt-5 gap-2 text-center>
     <button icon-btn @click="toggleDark()">
       <div dark:i-carbon-moon i-carbon-sun />
     </button>
