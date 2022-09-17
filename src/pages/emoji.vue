d3328ebb07eedf4f396e30891a096560056d219f
Yanyeyeye <1024079655@qq.com>
2022-09-17 21:41:04 +0800

    feat: add random  emoji and  PNG image exporting

--- src/pages/emoji.vue
+++ src/pages/emoji.vue
@@ -30,8 +30,8 @@ type ImportModuleFunction = () => Promise<SvgImageModule> // 返回Promise resol
 
 const el = $ref<HTMLCanvasElement>()
 const ctx = $computed(() => el.getContext('2d')!)
-const WIDTH = 1080
-const HEIGHT = 1080
+const WIDTH = 270
+const HEIGHT = 270
 
 // 得到图片的路径
 async function loadImageArray(params: Record<string, ImportModuleFunction>) {
@@ -62,6 +62,7 @@ async function getImages() {
   const modulesDetails = import.meta.glob<SvgImageModule>('../assets/emoji/details/*.svg')
   const Details = await loadImageArray(modulesDetails)
   emoji.details = ['', ...Details]
+  randRomEmoji()
 }
 
 // 生成图片标签
@@ -71,12 +72,32 @@ function generateImages(path: string) {
       resolve(null)
     const img = new Image(400, 400)
     img.src = path
-    img.onload = (e) => {
+    img.onload = () => {
       resolve(img)
     }
   })
 }
 
+// 随机取数
+function randomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min // 含最大值，含最小值
+}
+
+// 随机生成emoji
+function randRomEmoji() {
+  const headLength = emoji.heads.length - 1
+  const eyebrowLength = emoji.eyebrows.length - 1
+  const eyeLength = emoji.eyes.length - 1
+  const mouthLength = emoji.mouths.length - 1
+  const detailLength = emoji.details.length - 1
+  SelectEmoji.heads = emoji.heads[randomNumber(0, headLength)]
+  SelectEmoji.eyebrows = emoji.eyebrows[randomNumber(0, eyebrowLength)]
+  SelectEmoji.eyes = emoji.eyes[randomNumber(0, eyeLength)]
+  SelectEmoji.mouths = emoji.mouths[randomNumber(0, mouthLength)]
+  SelectEmoji.details = emoji.details[randomNumber(0, detailLength)]
+}
+
+// 监听
 watch(SelectEmoji, () => {
   const headPath = SelectEmoji.heads
   const eyesPath = SelectEmoji.eyes
@@ -90,8 +111,11 @@ watch(SelectEmoji, () => {
     generateImages(mouthPath),
     generateImages(detailPath),
   ]).then((images) => {
+    // 清除画布
     ctx.clearRect(0, 0, el.width, el.height)
+    // 遍历得到的图片集
     images.forEach((img) => {
+      // 画emoji的身体各个部分
       img && ctx.drawImage(img, 0, 0, WIDTH, HEIGHT)
     })
     el.classList.add('animation')
@@ -99,9 +123,27 @@ watch(SelectEmoji, () => {
       el.classList.remove('animation')
     }, 500)
   })
-},
+})
 
-)
+// 导出PNG图片
+function exportPNG() {
+  el.toBlob((blob: Blob | null) => {
+    const PNG = document.createElement('a')
+    const url = URL.createObjectURL(blob!)
+    PNG.href = url
+    PNG.download = `emoji_${Date.now()}`
+    PNG.click()
+  })
+}
+
+// 导出SVG格式图片
+// async function exportSVG() {
+//   TODO
+//   const parser = new DOMParser()
+//   // 将svg转成svg文档(text)
+//   const documents = await Promise.all(Object.values(SelectEmoji).map(imageUrl => fetch(imageUrl).then(res => res.text())))
+//   // 将每一项svg装换成dom标签
+// }
 
 onMounted(() => {
   getImages()
@@ -109,71 +151,80 @@ onMounted(() => {
 </script>
 
 <template>
-  <div font-800 mb-2 p-5 text-size-8>
+  <div font-800 mb-5 text="size-8 center">
     Create Emoji you Like
   </div>
-  <canvas
-    ref="el"
-    w-150px
-    h-150px
-    width="1080"
-    height="1080"
-    m-auto
-    border="~ rounded-5"
-    bg-gray-100
-    dark:bg-neutral-600
-    class="animation"
-  />
-  <div m-auto>
-    <h2 text-sm font-bold mt-3>
-      Head
-    </h2>
-    <div flex flex-wrap justify-center>
-      <div v-for="(item, index) in emoji.heads" :key="index">
-        <Section @click="SelectEmoji.heads = item">
-          <img :src="item">
-        </Section>
-      </div>
+  <div relative w-150px h-150px m-auto>
+    <canvas
+      ref="el" w-150px h-150px width="270" height="270" border="~ rounded-5" bg-gray-100 dark:bg-neutral-600
+      class="animation"
+    />
+    <div btn h-10 w-10 p2 absolute left-125px bottom--10px @click="randRomEmoji">
+      <div i-mdi-refresh text-2xl />
     </div>
-    <h2 text-sm font-bold mt-3>
-      Eyebrows
-    </h2>
-    <div flex flex-wrap justify-center>
-      <div v-for="(item, index) in emoji.eyebrows" :key="index">
-        <Section @click="SelectEmoji.eyebrows = item">
-          <img :src="item">
-        </Section>
-      </div>
+  </div>
+  <div mt-5 text-center relative>
+    <div btn h-10 @click="exportPNG">
+      <div i-mdi-export text-xl />
+      <span font-bold pl-2>
+        Export PNG
+      </span>
     </div>
-    <h2 text-sm font-bold mt-3>
-      Eyes
-    </h2>
-    <div flex flex-wrap justify-center>
-      <div v-for="(item, index) in emoji.eyes" :key="index">
-        <Section @click="SelectEmoji.eyes = item">
-          <img :src="item">
-        </Section>
-      </div>
+    <!-- <div btn h-10 ml-5 @click="exportSVG">
+      <div i-mdi-export text-xl />
+      <span font-bold pl-2>
+        Export SVG
+      </span>
+    </div> -->
+  </div>
+  <h2 text-lg font-bold mt-3 pl-2>
+    Head
+  </h2>
+  <div flex="~ wrap">
+    <div v-for="(item, index) in emoji.heads" :key="index">
+      <Section @click="SelectEmoji.heads = item">
+        <img :src="item">
+      </Section>
     </div>
-    <h2 text-sm font-bold mt-3>
-      Mouths
-    </h2>
-    <div flex flex-wrap justify-center>
-      <div v-for="(item, index) in emoji.mouths" :key="index">
-        <Section @click="SelectEmoji.mouths = item">
-          <img :src="item">
-        </Section>
-      </div>
+  </div>
+  <h2 text-lg font-bold mt-3 pl-2>
+    Eyebrows
+  </h2>
+  <div flex="~ wrap">
+    <div v-for="(item, index) in emoji.eyebrows" :key="index">
+      <Section @click="SelectEmoji.eyebrows = item">
+        <img :src="item">
+      </Section>
     </div>
-    <h2 text-sm font-bold mt-3>
-      Details
-    </h2>
-    <div flex flex-wrap justify-center>
-      <div v-for="(item, index) in emoji.details" :key="index">
-        <Section @click="SelectEmoji.details = item">
-          <img :src="item">
-        </Section>
-      </div>
+  </div>
+  <h2 text-lg font-bold mt-3 pl-2>
+    Eyes
+  </h2>
+  <div flex="~ wrap">
+    <div v-for="(item, index) in emoji.eyes" :key="index">
+      <Section @click="SelectEmoji.eyes = item">
+        <img :src="item">
+      </Section>
+    </div>
+  </div>
+  <h2 text-lg font-bold mt-3 pl-2>
+    Mouths
+  </h2>
+  <div flex="~ wrap">
+    <div v-for="(item, index) in emoji.mouths" :key="index">
+      <Section @click="SelectEmoji.mouths = item">
+        <img :src="item">
+      </Section>
+    </div>
+  </div>
+  <h2 text-lg font-bold mt-3 pl-2>
+    Details
+  </h2>
+  <div flex="~ wrap">
+    <div v-for="(item, index) in emoji.details" :key="index">
+      <Section @click="SelectEmoji.details = item">
+        <img :src="item">
+      </Section>
     </div>
   </div>
 </template>
