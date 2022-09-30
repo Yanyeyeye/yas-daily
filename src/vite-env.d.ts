356da83a86b349f81ab00bb62322cf34da2f3a66
Yanyeyeye <1024079655@qq.com>
2022-09-30 16:05:58 +0800

    chore: initial 3d

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,6 @@
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vue/macros-global" />
 
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
