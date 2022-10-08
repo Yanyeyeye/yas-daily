a384b22668dda2382d2c313de49b8be1b9d2a96a
Yanyeyeye <1024079655@qq.com>
2022-10-08 18:09:55 +0800

    feat: add spotlight and ground for shadow

--- src/pages/crown.vue
+++ src/pages/crown.vue
@@ -3,25 +3,66 @@ import * as t from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new t.Scene()
-const geometry = new t.SphereGeometry(80, 50, 25)
-const material = new t.PointsMaterial({
-  color: 0xE5C07B,
-  size: 3,
+
+const geometry = new t.BoxGeometry(100, 100, 100) // 创建一个立方体几何对象Geometry
+// 点渲染模式
+const material = new t.MeshLambertMaterial({
+  color: 0x906EFE,
+  transparent: true,
+  opacity: 0.8,
+}) // 材质对象
+const faces = new t.Mesh(geometry, material)
+scene.add(faces)
+
+// 设置产生投影的网格模型
+faces.castShadow = true
+
+// 创建一个平面几何体作为投影面
+const planeGeometry = new t.PlaneGeometry(600, 700)
+const planeMaterial = new t.MeshLambertMaterial({
+  color: 0x808080,
 })
-const point = new t.Points(geometry, material) // 网格对象
-scene.add(point)
+// 平面网格模型作为投影面
+const planeMesh = new t.Mesh(planeGeometry, planeMaterial)
+scene.add(planeMesh) // 网格模型添加到场景中
+planeMesh.rotateX(-Math.PI / 2) // 旋转网格模型
+planeMesh.position.y = -50 // 设置网格模型y坐标
+// 设置接收阴影的投影面
+planeMesh.receiveShadow = true
 
 // 坐标轴
 // const axesHelper = new t.AxesHelper(300)
 // scene.add(axesHelper)
 
 // 灯光
-const lightPoint = new t.PointLight(0xFFFFFF)
-lightPoint.position.set(400, 200, 300)
-scene.add(lightPoint)
+// const lightPoint = new t.PointLight(0xFFFFFF)
+// lightPoint.position.set(100, 200, 300)
+// scene.add(lightPoint)
+
+// const sphereSize = 10
+// const pointLightHelper = new t.PointLightHelper(lightPoint, sphereSize)
+// scene.add(pointLightHelper)
+
+// 聚光顶
+const spotLight = new t.SpotLight(0xFFFFFF)
+// 设置光源位置
+spotLight.position.set(100, 200, 200)
+spotLight.penumbra = 1
+spotLight.angle = Math.PI / 10
+scene.add(spotLight)
+// 光源投影
+spotLight.castShadow = true
+// 设置计算阴影的区域，注意包裹对象的周围
+spotLight.shadow.camera.near = 10
+spotLight.shadow.camera.far = 20
+spotLight.shadow.focus = 1
+
+// 聚光灯辅助对象
+const lightHelper = new t.SpotLightHelper(spotLight)
+scene.add(lightHelper)
 
 // 自然光
-const envLight = new t.AmbientLight(0x404040)
+const envLight = new t.AmbientLight(0x888888)
 scene.add(envLight)
 
 const WIDTH = window.innerWidth
@@ -35,8 +76,9 @@ camera.lookAt(scene.position)
 
 const renderer = new t.WebGLRenderer()
 renderer.setSize(WIDTH, HEIGHT)
-renderer.setClearColor(0x000, 0.7)
+renderer.setClearColor(0x000, 0.9)
 renderer.render(scene, camera)
+renderer.shadowMap.enabled = true
 
 onMounted(() => {
   document.getElementById('crown')!.appendChild(renderer.domElement)
@@ -55,7 +97,7 @@ function animate() {
   T0 = T1
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-  point.rotateY(0.0003 * t)
+  faces.rotateY(0.0003 * t)
 }
 animate()
 </script>
